44a3b70430d62575ac502970267d6b688ab7b67f
Sarah2603 <sarahbarreto86@gmail.com>
2022-11-04 09:50:31 -0300

    deploy

---
+++ src/users/users.service.spec.ts
@@ -0,0 +1,19 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { EmailService } from 'src/email/email.service';
+import { UsersService } from './users.service';
+
+describe('UsersService', () => {
+  let service: UsersService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [UsersService, EmailService]
+    }).compile();
+
+    service = module.get<UsersService>(UsersService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
